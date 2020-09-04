ffb04be0655132f28d1a01d3878d02bc8cb9e9fd
Marina <mansumdp@gmail.com>
2020-09-04 14:03:12 -0300

    agrego librerias

---
+++ scripts.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="librerias/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatables/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
+
+<script src="librerias/jquery/jquery.min.js"></script>
+<script src="librerias/bootstrap/popper.min.js"></script>
+<script src="librerias/bootstrap/bootstrap.min.js"></script>
+<script src="librerias/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="librerias/datatables/jquery.dataTables.min.js"></script>
+<script src="librerias/alertify/alertify.js"></script>
