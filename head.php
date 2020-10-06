daca4ab1efda9059faac0ec5528baa3fed509774
mari <mansumdp@gmail.com>
2020-10-06 17:17:36 -0300

    conexion con bd usando ajax

--- head.php
+++ head.php
@@ -4,9 +4,9 @@
     <link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
     <link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
 
-    <script src="librerias/jquery/jquery.min.js"></script>
+<!--     <script src="librerias/jquery/jquery.min.js"></script>
     <script src="librerias/bootstrap/popper.min.js"></script>
     <script src="librerias/bootstrap/bootstrap.min.js"></script>
     <script src="librerias/datatables/jquery.dataTables.min.js"></script>
-    <script src="librerias/alertify/alertify.js"></script>
+    <script src="librerias/alertify/alertify.js"></script> -->
 </head>
